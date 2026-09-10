4576df2302c34832a03c52e0bd6dcadb350fcfda
Farhan Shamshad <126975284+Farhan3112@users.noreply.github.com>
2026-09-10 23:39:56 +0530

    changed port

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const connectToDB = require('./src/config/database')
 
 connectToDB()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
